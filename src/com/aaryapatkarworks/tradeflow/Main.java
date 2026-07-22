53f93338d91295621aecac5093be8c9cf0e3aeb0
aaryapatkarworks <aaryapatkarworks@gmail.com>
2026-07-22 14:55:51 +0530

    feat: implement User model

--- src/com/aaryapatkarworks/tradeflow/Main.java
+++ src/com/aaryapatkarworks/tradeflow/Main.java
@@ -1,17 +1,27 @@
 package com.aaryapatkarworks.tradeflow;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        User user1 = new User(
+                101,
+                "Aarya Patkar",
+                "aarya210@gmail.com",
+                "password21",
+                150000
+        );
+
+        User user2 = new User(
+                102,
+                "Pranav More",
+                "pranav007@gmail.com",
+                "password10",
+                100000
+        );
+
+        user1.displayUser();
+        System.out.println();
+        user2.displayUser();
     }
 }
--- src/com/aaryapatkarworks/tradeflow/User.java
+++ src/com/aaryapatkarworks/tradeflow/User.java
@@ -1,4 +1,32 @@
 package com.aaryapatkarworks.tradeflow;
 
 public class User {
+
+    int userId;
+    String fullName;
+    String email;
+    String password;
+    double walletBalance;
+
+    User(int userId,
+         String fullName,
+         String email,
+         String password,
+         double walletBalance) {
+
+        this.userId = userId;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.walletBalance = walletBalance;
+    }
+
+    void displayUser() {
+
+        System.out.println("========== USER ==========");
+        System.out.println("ID      : " + userId);
+        System.out.println("Name    : " + fullName);
+        System.out.println("Email   : " + email);
+        System.out.printf("Wallet  : ₹%.2f%n", walletBalance);
+    }
 }
